aae80858c96416e552805ce91b731fd9423cd864
kabudafax <lewisnie123@163.com>
2024-05-04 10:31:36 +0800

    feat: add funded contract

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -3,11 +3,17 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
-  function run() external {
+  function run() external returns (FundMe) {
+    //在broadcast之前的交易都不会在真实网络上进行
+    HelperConfig helperConfig = new HelperConfig();
+    address ethusdPriceFeed = helperConfig.activeNetworkConfig();
+
     vm.startBroadcast();
-    new FundMe();
+    FundMe fundMe = new FundMe(ethusdPriceFeed);
     vm.stopBroadcast();
+    return fundMe;
   }
 }
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -5,12 +5,21 @@ import {Test, console} from "forge-std/Test.sol";
 // import "forge-std/Test.sol";
 
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
   FundMe fundMe;
+  // 伪造一个发送合约的地址,这样与任何人一起工作时，都可以使用这个伪造的地址
+  address USER = makeAddr("user");
+  uint256 constant SEND_VALUE = 0.1 ether;
+  uint256 constant STARTING_BALANCE = 10 ether;
+  uint256 constant GAS_PRICE = 1;
 
   function setUp() external {
-    fundMe = new FundMe();
+    // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    DeployFundMe deployFundMe = new DeployFundMe();
+    fundMe = deployFundMe.run();
+    vm.deal(USER, STARTING_BALANCE);
   }
 
   function testMinimumDollarIsFive() public view {
@@ -19,7 +28,91 @@ contract FundMeTest is Test {
 
   // 这里fundMe合约的部署者是test合约，而我们正在调用test合约，所以msg.sender是我，因此两者不一致。要用address（this）表示此合约的地址
   function testOwnerIsMsgSender() public view {
-    // assertEq(fundMe.i_owner(), msg.sender);
-    assertEq(fundMe.i_owner(), address(this));
+    assertEq(fundMe.getOwner(), msg.sender);
+    // assertEq(fundMe.i_owner(), address(this));
+  }
+
+  function testPricefeedVersionIsAccurate() public view {
+    uint256 version = fundMe.getVersion();
+    assertEq(version, 4);
+  }
+
+  function testFundFailsWithoutEnoughETH() public {
+    vm.expectRevert(); // the next line should revert!
+    fundMe.fund();
+  }
+
+  function testFundUpdatesFundedDataStructure() public {
+    vm.prank(USER); // 告诉虚拟机下一笔交易由USER发送
+    fundMe.fund{value: SEND_VALUE}();
+    // uint256 amountFunded = fundMe.getAddressToAmountFunded(address(this));
+    uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+    assertEq(amountFunded, SEND_VALUE);
+  }
+
+  function testAddsFunderToArrayOfFunders() public {
+    vm.prank(USER);
+    fundMe.fund{value: SEND_VALUE}();
+    address funder = fundMe.getFunder(0);
+    assertEq(funder, USER);
+  }
+
+  modifier funded() {
+    vm.prank(USER);
+    fundMe.fund{value: SEND_VALUE}();
+    _;
+  }
+  function testOnlyownerCanWithdraw() public funded {
+    vm.expectRevert();
+    vm.prank(USER);
+    fundMe.withdraw();
+  }
+
+  function testWithDrawWithASingleFunder() public funded {
+    // 遵循arrange、act、assert的模式
+    // Arrange， 安排测试，设置测试环境
+    uint256 startingOwnerBalance = fundMe.getOwner().balance;
+    uint256 startingFundMeBalance = address(fundMe).balance;
+
+    // Act，执行想要进行测试的操作
+    uint256 gasStart = gasleft();
+    vm.txGasPrice(GAS_PRICE);
+    vm.prank(fundMe.getOwner());
+    fundMe.withdraw();
+    uint256 gasEnd = gasleft();
+    uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+    console.log(gasUsed);
+
+    // Assert，断言测试
+    uint256 endingOwnerBalance = fundMe.getOwner().balance;
+    uint256 endingFundMeBalance = address(fundMe).balance;
+    assertEq(endingFundMeBalance, 0);
+    assertEq(startingFundMeBalance + startingOwnerBalance, endingOwnerBalance);
+  }
+
+  function testWithdrawFromMultipleFunders() public funded {
+    // Arrange，solidity中地址是uint160类型，不能使用uint256
+    uint160 numberOffunders = 10;
+    uint160 startingFunderIndex = 1;
+    for (uint160 i = startingFunderIndex; i < numberOffunders; i++) {
+      hoax(address(i), SEND_VALUE);
+      // vm.prank(address(i));
+      // vm.deal(address(i), SEND_VALUE);
+      // hoax会prank一个地址并赋予一些初始的ether
+      fundMe.fund{value: SEND_VALUE}();
+    }
+    uint256 startingOwnerBalance = fundMe.getOwner().balance;
+    uint256 startingFundMeBalance = address(fundMe).balance;
+
+    // Act
+    vm.startPrank(fundMe.getOwner());
+    fundMe.withdraw();
+    vm.stopPrank();
+
+    // Assert
+    assert(address(fundMe).balance == 0);
+    assert(
+      startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance
+    );
   }
 }
